916baa182a56e2ee8211cd22d6db1b24bf602481
Maggie-Karuri <karurimargaret111@gmail.com>
2024-07-01 02:01:01 +0300

    Getting Remote Data

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,29 @@ import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
-
+    
     def get_response_body(self):
-        pass
-
+        response = requests.get(self.url)
+        return response.content
+    
     def load_json(self):
-        pass
+        response = self.get_response_body()
+        return json.loads(response)
+    
+    def get_user_input(self, prompt):
+        user_input = input(prompt)
+        return user_input
+    
+    def get_user_search_results(self, search_term):
+        search_term_formatted = search_term.replace(" ", "+")
+        fields = ["title", "author_name"]
+        fields_formatted = ",".join(fields)
+        limit = 1
+
+        url = f"https://openlibrary.org/search.json?title={search_term_formatted}&fields={fields_formatted}&limit={limit}"
+        
+        response = requests.get(url).json()
+        response_formatted = f"Title: {response['docs'][0]['title']}\nAuthor: {response['docs'][0]['author_name'][0]}"
+        return response_formatted
